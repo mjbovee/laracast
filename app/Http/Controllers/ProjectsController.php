b9e7615d9675a54fb7987d21b4871f04bfe9adc7
Michael Bovee <michael.j.bovee@gmail.com>
2019-01-31 13:09:24 -0700

    more MVC!

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index() {
+        $projects = \App\Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+}
