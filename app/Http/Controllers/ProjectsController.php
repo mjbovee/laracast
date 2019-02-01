03ba6beb46eba12a247539bdcb51c183d1120d66
Michael Bovee <michael.j.bovee@gmail.com>
2019-02-01 11:36:10 -0700

    cleaned up controller

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -16,40 +16,28 @@ class ProjectsController extends Controller
         return view('projects.create');
     }
 
-    public function show() {
-        
+    public function show(Project $project) {
+        return view('projects.show', compact('project'));
     }
 
-    public function edit($id) {
-        $project = Project::findOrFail($id);
-
+    public function edit(Project $project) {
         return view('projects.edit', compact('project'));
     }
 
-    public function update($id) {
-        $project = Project::findOrFail($id);
-
-        $project->title = request('title');
-        $project->description = request('description');
-
-        $project->save();
+    public function update(Project $project) {
+        $project->update(request(['title', 'description']));
 
         return redirect('/projects');
     }
 
-    public function destroy($id) {
-        Project::findOrFail($id)->delete();
+    public function destroy(Project $project) {
+        $project->delete();
 
         return redirect('/projects');
     }
 
     public function store() {
-        $project = new Project();
-
-        $project->title = request('title');
-        $project->description = request('description');
-
-        $project->save();
+        Project::create(request(['title', 'description']));
 
         return redirect('/projects');
     }
