a7edbd380ac1cb6f7a8d0a93f7909890e22840de
Michael Bovee <michael.j.bovee@gmail.com>
2019-02-01 09:25:17 -0700

    added form and create project view, altered project controller

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Project;
 
 class ProjectsController extends Controller
 {
     public function index() {
-        $projects = \App\Project::all();
+        $projects = Project::all();
 
         return view('projects.index', compact('projects'));
+    }
+
+    public function create() {
+        return view('projects.create');
+    }
+
+    public function store() {
+        $project = new Project();
+
+        $project->title = request('title');
+        $project->description = request('description');
+
+        $project->save();
+
+        return redirect('/projects');
     }
 }
---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Create a new Project</h1>
+
+    <form method="POST" action="/projects">
+        {{ csrf_field() }}
+
+        <div>
+            <input type="text" name="title" placeholder="Project title">
+        </div>
+
+        <div>
+            <textarea name="description" placeholder="Project description"></textarea>
+        </div>
+
+        <div>
+            <button type="submit">Create Project</button>
+        </div>
+    </form>
+</body>
+</html>
